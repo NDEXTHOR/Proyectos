2bfada60d93410c9d4c22ae711e3c4bcc3c40c65
Brayan Rios <bjrf81440@gmail.com>
2026-02-25 07:30:26 -0600

    No se que hice

---
+++ GaussSimple.py
@@ -0,0 +1,5 @@
+# Con este programa se espera solucionar sistemas de ecuaciones lineales con el metodo Gauss simple(Talvez se agregue Gauss Jordan)
+# Para simplificar la entrada sera la matriz y no el sistema tal cual
+
+tam = input("Ingresa el tamaño: ")
+
