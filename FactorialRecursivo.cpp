318a45a6823ab153e534a778ea4a4caeb762c3db
Brayan Rios <bjrf81440@gmail.com>
2026-02-28 16:50:38 -0600

    FactoriaC++

---
+++ FactorialRecursivo.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int factorial(int n){
+    return (n == 1 || n == 0) ? 1 : n * factorial(n-1);
+}
+
+int main(){
+    cout << "Ingresa un numero: ";
+    cin >> n;
+    cout << factorial(n);
+    return 0;
+}
