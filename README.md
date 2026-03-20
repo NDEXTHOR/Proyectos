3c933443543af053455cae0859c7ec6cba74a668
Brayan Rios <bjrf81440@gmail.com>
2026-03-19 22:01:54 -0600

    Sudoku

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Projects - Operador de matrices
 
-El operador es bastante basico, solo hace sumas, resas y multiplicaciones, algo que se debe tener en cuenta es que no tiene el mejor formato, no puestra procedimiento o algo parecido y no cuenta con validaciones, pero en si haces las operaciones correctamente
+El operador es bastante basico, solo hace sumas, resas y multiplicaciones, algo que se debe tener en cuenta es que no tiene el mejor formato, no muestra procedimiento o algo parecido y no cuenta con validaciones, pero en si haces las operaciones correctamente
---
+++ Sudoku.cpp
@@ -0,0 +1,281 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+class sudoku {
+private:
+	char matriz[9][9];// Un sudoku tiene 81 espacios
+    int F_Inicio = 0;
+    int C_Inicio = 0;
+
+public:	
+	sudoku();// La matriz se inicializa con espacios vacios
+	sudoku(char mtzUsuario[9][9]);// La matriz se inicializa con una matriz definida
+
+	void imprimir();
+    
+    // Reciben un numero y numero de fila, columna o region y verifican si ese numero falta en esa fila, columna o region
+	bool verificarFila(char numAux, int noFila);
+	bool verificarColumna(char numAux, int noColumna);
+	bool verificarRegion(char numAux, int region);
+
+    // Verifica si el sudoku esta bien resuelto
+    bool verificarResolucion();
+	
+    friend class problemaSudoku;
+};
+
+sudoku::sudoku() {
+	//Inicializar la matriz con espacios vacíos, cuando se imprime no se ve nada jaja
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			matriz[i][j] = ' ';
+		}
+	}
+}
+sudoku::sudoku(char mtzUsuario[9][9]) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            matriz[i][j] = mtzUsuario[i][j];// Se iguala a la matriz definida
+        }
+    }
+}
+
+void sudoku::imprimir() {  
+    for (int i = 0; i < 9; i++) {  
+        cout << "- - - - - - - - - - - - - - - - - - -" << endl;// Supongo que podria mejorar el formato, pero asi se ve bien
+        for (int j = 0; j < 9; j++) {  
+            if (j < 8) {
+                cout << "| " << matriz[i][j] << " ";
+            } else {
+                cout << "| " << matriz[i][j] << " |";
+            }
+        }  
+        cout << endl; 
+    }  
+    cout << "- - - - - - - - - - - - - - - - - - -" << endl;
+}
+
+bool sudoku::verificarFila(char numAux, int noFila) {
+    for (int i = 0; i < 9; i++) {
+        if (matriz[noFila][i] == numAux) {
+            return true;
+        }
+    }
+	return false;
+}
+bool sudoku::verificarColumna(char numAux, int noColumna) {
+    for (int i = 0; i < 9; i++) {
+        if (matriz[i][noColumna] == numAux) {
+            return true;
+        }
+    }
+    return false;
+}
+bool sudoku::verificarRegion(char numAux, int region) {
+    // Queria usar un switch, pero no se por que no funciono
+    if (region == 1) {
+        F_Inicio = 0;
+        C_Inicio = 0;
+    } else if (region == 2) {
+        F_Inicio = 0;
+        C_Inicio = 3;
+    } else if (region == 3) {
+        F_Inicio = 0;
+        C_Inicio = 6;
+    } else if (region == 4) {
+        F_Inicio = 3;
+        C_Inicio = 0;
+    } else if (region == 5) {
+        F_Inicio = 3;
+        C_Inicio = 3;
+    } else if (region == 6) {
+        F_Inicio = 3;
+        C_Inicio = 6;
+    } else if (region == 7) {
+        F_Inicio = 6;
+        C_Inicio = 0;
+    } else if (region == 8) {
+        F_Inicio = 6;
+        C_Inicio = 3;
+    } else if (region == 9) {
+        F_Inicio = 6;
+        C_Inicio = 6;
+    }
+
+    for (int i = F_Inicio; i < F_Inicio + 3; i++) {
+        for (int j = C_Inicio; j < C_Inicio + 3; j++){
+            if (matriz[i][j] == numAux) {
+                return true;
+            }
+        }
+    }
+	return false;
+}
+
+bool sudoku::verificarResolucion() {
+    // Verificar filas
+    for (int fila = 0; fila < 9; fila++) {// Numero de fila
+        for (char num = '1'; num <= '9'; num++) {// Numero a verificar  -  No sabia que se podrian iterar caracteres
+            if (!verificarFila(num, fila)) {
+                return false;
+            }
+        }
+    }
+
+    // Verificar columnas
+    for (int col = 0; col < 9; col++) {// Numero de fila
+        for (char num = '1'; num <= '9'; num++) {// Numero a verificar
+            if (!verificarColumna(num, col)) {
+                return false;
+            }
+        }
+    }
+
+    // Verificar regiones
+    for (int region = 1; region <= 9; region++) {// Numero de fila
+        for (char num = '1'; num <= '9'; num++) {// Numero a verificar
+            if (!verificarRegion(num, region)) {
+                return false;
+            }
+        }
+    }
+
+    return true; // Si pasa todas las verificaciones
+}
+
+class problemaSudoku {
+private:
+    sudoku inicial;
+public:
+    problemaSudoku(sudoku);
+    void Amplitud();
+    void Profundidad();
+};
+
+problemaSudoku::problemaSudoku(sudoku a) {
+    inicial = a;
+    cout << "Sudoku a resolver: " << endl;
+    inicial.imprimir();
+}
+
+void problemaSudoku::Amplitud() {
+    list<sudoku> lista;
+
+    int iteracion = 1;
+    bool resuelto = false;
+
+    lista.push_front(inicial);
+
+    while (!lista.empty() && !resuelto) {
+        sudoku aux = lista.front();
+        lista.pop_front();
+
+        if (aux.verificarResolucion()) {
+            cout << "Se encontro la resolucion con busqueda por amplitud en " << iteracion <<" iteraciones..." << endl;
+            aux.imprimir();
+            resuelto = true;
+            break;
+        }
+
+        // Buscar la primera celda vacía
+        bool celdaVaciaEncontrada = false;
+        for (int i = 0; i < 9 && !celdaVaciaEncontrada; i++) {// No sabia que se podria hacer esto
+            for (int j = 0; j < 9 && !celdaVaciaEncontrada; j++) {
+                if (aux.matriz[i][j] == ' ') {
+                    // Probar números del 1 al 9
+                    for (char num = '1'; num <= '9'; num++) {
+                        int region = ((i / 3) * 3) + (j / 3) + 1;// Calcula la region
+                        if (!aux.verificarFila(num, i) &&
+                            !aux.verificarColumna(num, j) &&
+                            !aux.verificarRegion(num, region)) {
+                            sudoku nuevo(aux.matriz);
+                            nuevo.matriz[i][j] = num;
+                            lista.push_back(nuevo);
+                        }
+                    }
+                    celdaVaciaEncontrada = true; // Solo expandimos el primer vacío
+                }
+            }
+        }
+
+        iteracion++;
+    }
+
+    if (!resuelto) {
+        cout << "No se encontró solución." << endl;
+    }
+}
+void problemaSudoku::Profundidad() {
+    list<sudoku> lista;
+
+    int iteracion = 1;
+    bool resuelto = false;
+
+    lista.push_front(inicial);
+
+    while (!lista.empty() && !resuelto) {
+        sudoku aux = lista.back();
+        lista.pop_back();
+
+        if (aux.verificarResolucion()) {
+            cout << "Se encontro la resolucion con busqueda por profundidad en " << iteracion << " iteraciones..." << endl;
+            aux.imprimir();
+            resuelto = true;
+            break;
+        }
+
+        // Buscar la primera celda vacía
+        bool celdaVaciaEncontrada = false;
+        for (int i = 0; i < 9 && !celdaVaciaEncontrada; i++) {// No sabia que se podria hacer esto
+            for (int j = 0; j < 9 && !celdaVaciaEncontrada; j++) {
+                if (aux.matriz[i][j] == ' ') {
+                    // Probar números del 1 al 9
+                    for (char num = '1'; num <= '9'; num++) {
+                        int region = ((i / 3) * 3) + (j / 3) + 1;// Calcula la region
+                        if (!aux.verificarFila(num, i) &&
+                            !aux.verificarColumna(num, j) &&
+                            !aux.verificarRegion(num, region)) {
+                            sudoku nuevo(aux.matriz);
+                            nuevo.matriz[i][j] = num;
+                            lista.push_back(nuevo);
+                        }
+                    }
+                    celdaVaciaEncontrada = true; // Solo expandimos el primer vacío
+                }
+            }
+        }
+
+        iteracion++;
+    }
+
+    if (!resuelto) {
+        cout << "No se encontró solución." << endl;
+    }
+}
+
+int main(){
+    // Matriz definida
+    char matrizAux[9][9] = {
+    { '8', ' ', ' ',   ' ', ' ', ' ',   ' ', ' ', ' ' },
+    { ' ', ' ', '3',   '6', ' ', ' ',   ' ', ' ', ' ' },
+    { ' ', '7', ' ',   ' ', '9', ' ',   '2', ' ', ' ' },
+
+    { ' ', '5', ' ',   ' ', ' ', '7',   ' ', ' ', ' ' },
+    { ' ', ' ', ' ',   ' ', '4', '5',   '7', ' ', ' ' },
+    { ' ', ' ', ' ',   '1', ' ', ' ',   ' ', '3', ' ' },
+
+    { ' ', ' ', '1',   ' ', ' ', ' ',   ' ', '6', '8' },
+    { ' ', ' ', '8',   '5', ' ', ' ',   ' ', '1', ' ' },
+    { ' ', '9', ' ',   ' ', ' ', ' ',   '4', ' ', ' ' }
+    };
+
+    sudoku a(matrizAux);// Constructora con argumentos
+
+    problemaSudoku A(a);
+
+    A.Amplitud();
+
+    A.Profundidad();
+}
